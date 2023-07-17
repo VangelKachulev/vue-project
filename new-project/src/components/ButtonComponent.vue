41657f51c6e1e4364657b3a6978ff992bfcbe8fb
VangelKachulev <vangel.kachulev@abv.bg>
2023-07-17 08:29:46 +0300

    btn fix

--- new-project/src/components/ButtonComponent.vue
+++ new-project/src/components/ButtonComponent.vue
@@ -1,9 +1,25 @@
 <template>
-    <button>Add something</button>
+    <button class="btn" :style="{background:color}">{{text}}</button>
 </template>
 
 <script>
 export default {
-    name: 'ButtonComponent'
+    name: 'ButtonComponent',
+    props: {
+        text: String,
+        color:String
+    }
 }
 </script>
+
+<style>
+.btn {
+    color: white;
+    background-color: grey;
+    border: 1px soid black;
+    border-radius: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    padding: 10px 12px;
+}
+</style>
--- new-project/src/components/HeaderComponent.vue
+++ new-project/src/components/HeaderComponent.vue
@@ -1,14 +1,18 @@
 <template>
   <header>
 
-    <nav>
+    <nav class="nav">
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
-      <ButtonComponent></ButtonComponent>
+      <div>
+        <ButtonComponent text="Log in" color="red"></ButtonComponent>
+        <ButtonComponent text="Sign in" color="red"></ButtonComponent>
+      </div>
+
     </nav>
   </header>
 </template>
@@ -19,14 +23,25 @@ import ButtonComponent from "./ButtonComponent.vue";
 
 export default {
   name: 'HeaderComponent',
+  props: {
+    title: String,
+  },
   components: {
     ButtonComponent,
-  }
+  },
 }
 </script>
 
 <style>
 /* Example CSS styles for the header */
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+ 
+}
+
 header {
   background-color: #333;
   color: #fff;
@@ -51,6 +66,7 @@ header nav ul li {
 header nav ul li a {
   color: #fff;
   text-decoration: none;
+  font-size: 20px;
 }
 
 header nav ul li a:hover {
